d4de9e78818afbdb0986c4452fdbbeb650f14099
Matthew Angel <matthewangel@Loaners-MacBook-Pro.local>
2021-12-05 23:05:53 -0500

    rendering html elements onto the page

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let searchValue = document.querySelector("#booksearch")
 let form = document.querySelector("#form")
 let searchInput = document.querySelector("#searchInput")
 let resultContent = document.querySelector("#resultContent")
+let result = document.querySelector("#result")
 
 
 function fetchBooks(){
@@ -10,8 +11,26 @@ function fetchBooks(){
   .then(data => {
 
 let dataArray = data.items
-console.log(dataArray)
-dataArray.forEach(book=> console.log(book.volumeInfo.title))
+console.log(dataArray[0].volumeInfo.imageLinks.thumbnail)
+for(let book of dataArray){
+  const bookImg = document.createElement("img");
+  const result = document.createElement("div");
+  const title = document.createElement("h1");
+  const subTitle = document.createElement("h3");
+  bookImg.setAttribute('src', `${book.volumeInfo.imageLinks.thumbnail}`)
+  subTitle.textContent = `${book.volumeInfo.subtitle}`
+  title.textContent = `Book Title: ${book.volumeInfo.title}`
+  result.style.marginBottom = '30px'
+  title.style.fontSize = '25px'
+  subTitle.style.fontSize = '15px'
+  resultContent.append(result)
+  result.append(bookImg)
+  result.appendChild(title)
+  result.appendChild(subTitle)
+
+}
+
+
   });
   
 }
