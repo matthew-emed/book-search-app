340c0e1405b3aea443b1fa97aa67d30322a9bea4
Matthew Angel <matthewangel@Loaners-MacBook-Pro.local>
2021-12-05 21:47:21 -0500

    added search functionality

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+let searchValue = document.querySelector("#booksearch")
+let form = document.querySelector("#form")
+let searchInput = document.querySelector("#searchInput")
+let resultContent = document.querySelector("#resultContent")
+
+
 function fetchBooks(){
-    fetch(' https://www.googleapis.com/books/v1/volumes?q=intitle:potter&key=AIzaSyCAeLhuL-3SApWpwdugiXCOO93F3utnx2w')
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput.value}&key=AIzaSyCAeLhuL-3SApWpwdugiXCOO93F3utnx2w`)
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+
+let dataArray = data.items
+console.log(dataArray)
+dataArray.forEach(book=> console.log(book.volumeInfo.title))
+  });
+  
 }
 
 
-fetchBooks()
+
+function logSubmit(e){
+  e.preventDefault()
+  console.log(searchInput.value)
+  
+  fetchBooks()
+}
+
+form.addEventListener('submit', logSubmit);
+
+
+
+
+
+// fetchBooks()
