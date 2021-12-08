d96699c9330465b2e86a82fa4721362d6ece9bae
Matthew Angel <matthewangel@Loaners-MacBook-Pro.local>
2021-12-07 23:41:54 -0500

    added author and subject search features app has bugs

--- index.js
+++ index.js
@@ -3,36 +3,46 @@ let form = document.querySelector("#form")
 let searchInput = document.querySelector("#searchInput")
 let resultContent = document.querySelector("#resultContent")
 let result = document.querySelector("#result")
+let orderBy = document.getElementsByTagName("select")[0].value
+let subject = document.querySelector("#searchSubject")
+let author = document.querySelector("#searchAuthor")
 
 
 function fetchBooks(){
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput.value}&key=AIzaSyCAeLhuL-3SApWpwdugiXCOO93F3utnx2w`)
+  fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchInput.value}+subject:${subject.value}+inauthor:${author.value}&maxResults=40&key=AIzaSyCAeLhuL-3SApWpwdugiXCOO93F3utnx2w`)
   .then(response => response.json())
   .then(data => {
-
+    console.log(author.value)
+console.log(data)
 let dataArray = data.items
 console.log(dataArray[0].volumeInfo.imageLinks.thumbnail)
 for(let book of dataArray){
+  console.log(book.volumeInfo.publishedDate)
+  const publishedDate = document.createElement("h4");
   const bookImg = document.createElement("img");
   const result = document.createElement("div");
   const title = document.createElement("h1");
   const subTitle = document.createElement("h3");
-  bookImg.setAttribute('src', `${book.volumeInfo.imageLinks.thumbnail}`)
+  const author = document.createElement("h4");
+  // console.log(book.volumeInfo.imageLinks.thumbnail);
+    bookImg.setAttribute('src', `${book.volumeInfo.imageLinks.thumbnail}`)
+   publishedDate.textContent = `${book.volumeInfo.publishedDate}`
+     author.textContent = `${book.volumeInfo.authors[0]}`
   subTitle.textContent = `${book.volumeInfo.subtitle}`
   title.textContent = `Book Title: ${book.volumeInfo.title}`
   result.style.marginBottom = '30px'
   title.style.fontSize = '25px'
   subTitle.style.fontSize = '15px'
   resultContent.append(result)
-  result.append(bookImg)
+   result.append(bookImg)
   result.appendChild(title)
   result.appendChild(subTitle)
+  result.appendChild(author)
+  result.appendChild(publishedDate)
+  result.style.borderBottom = '1px solid black'
 
 }
-
-
-  });
-  
+  })
 }
 
 
